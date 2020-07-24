ac634fd1c728c1391af52187c7b866375d74e8fd
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2020-07-24 16:28:42 +0530

    Add files via upload

---
+++ Alien.java
@@ -0,0 +1,11 @@
+public class Alien {
+    public String name;
+    public String getname()
+    {
+        return name;
+    }
+    public void setname(String name)
+    {
+        this.name=name;
+    }
+}
---
+++ second.kt
@@ -0,0 +1,6 @@
+fun main(args : Array<String >)
+{
+    var uttam= Alien();
+    uttam.name="msutafa";
+    println("Name of the worker is "+uttam.name)
+}
